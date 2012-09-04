dc16ad0af21cc89aece35d0e37e26f753890b380
Lars Erik Finholt <lars.erik.finholt@gmail.com>
2012-09-04 21:45:43 +0200

    First

---
+++ Scripts/main.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="knockout-2.1.0.debug.js" />
+var Task = function (title) {
+    this.title = title;
+    this.doneBy = ko.observable("");
+    this.done = ko.computed(function () {
+        return this.doneBy().length > 0?"btn-primary":"btn-warning";
+    },this);
+};
+
+
+
+var ViewModel = function (days, tasks, persons) {
+
+    this.days = ko.observableArray(days);
+    this.tasks = ko.observableArray(tasks);
+    this.persons = ko.observableArray(persons);
+
+};
+
+ko.applyBindings(new ViewModel( 
+    ["Mon","Tue","Wed","Thu","Fri","Sat"], 
+    [new Task("Morgentur"),new Task("Etterskoletur"), new Task("Ettermiddagstur"), new Task("Kveldstur")],
+    ["Lars Erik", "Camilla", "Markus", "Sigrid", "Ingvild"]));
