a85f117ebd69c5b2335e4911675484349775eb04
Lars Erik Finholt <lars.erik.finholt@gmail.com>
2012-09-08 20:52:39 +0200

    funker litt

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,34 +1,68 @@
 ﻿/// <reference path="knockout-2.1.0.debug.js" />
-var Task = function (title) {
-    this.title = title;
+var Task = function (title, date, id) {
+    this.date = date;
+    this.title = ko.observable(title);
+    this.id = id;
     this.doneBy = ko.observable("");
+    this.setDone = function (name) {
+        //this.title(name);
+        this.doneBy(name);
+    }
     this.done = ko.computed(function () {
         return this.doneBy().length > 0?"btn-primary":"btn-warning";
     },this);
-};
+}.bind(this);
+
+var Day = function (date, tasks) {
+    this.date = date;
+    this.tasks = ko.observableArray(tasks);
+}.bind(this);
 
 
 
-var ViewModel = function (days, tasks, persons) {
+var ViewModel = function (days, persons) {
 
     this.days = ko.observableArray(days);
-    this.tasks = ko.observableArray(tasks);
     this.persons = ko.observableArray(persons);
+
     this.showDialog = function (task, event) {
+        if (task.doneBy().length>0) {
+            alert("Already done by " + task.doneBy());
+            return;
+        }
         $('#myModal').modal('show');
         $('.personbutton').data('task', task);
         $('.personbutton').data('day', $(event.target).data('day'));
     };
+
     this.setDoneBy = function (person, event) {
-        alert(person + $(event.target).data('task').title + $(event.target).data('day'));
-        $(event.target).data('task').doneBy(person);
+ //       alert(person + $(event.target).data('task').title + $(event.target).data('day'));
+        $(event.target).data('task').setDone(person);
+        $('#myModal').modal('hide');
     };
 
 };
 
-ko.applyBindings(new ViewModel( 
-    ["Mon","Tue","Wed","Thu","Fri","Sat"], 
-    [new Task("Morgentur"),new Task("Etterskoletur"), new Task("Ettermiddagstur"), new Task("Kveldstur")],
-    ["Lars Erik", "Camilla", "Markus", "Sigrid", "Ingvild"]
-    )
-    );
+function start() {
+    var days = new Array();
+    var tasksTitles = ["Morgentur", "Etterskoletur", "Kveldstur"];
+    var id = 0;
+    for (var i = 0; i < 3; i++) {
+        var tasks = new Array();
+        var date = new Date(2012, 09, 1 + i);
+        for (var j = 0; j < tasksTitles.length; j++) {
+            tasks.push(new Task(tasksTitles[j], date, id++));
+        }
+        days.push(new Day(date, tasks));
+    }
+    var vm = new ViewModel(days,
+        ["Lars Erik", "Camilla", "Markus", "Sigrid", "Ingvild"]
+        );
+    $('#tmp').click(function () {
+        var jsonData = ko.toJSON(vm);
+        alert(jsonData);
+    });
+    ko.applyBindings(vm);
+};
+
+start();
